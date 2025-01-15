490a6ce6eb22f6d05dbcc445b845f5948c5462b3
Rahul Yadav <rahul.yadav6131766@gmail.com>
2025-01-15 09:04:10 +0530

    Added Header Section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,46 @@
-export const Navbar = () => {
-  return null;
+import Image from "next/image";
+
+import ArrowRight from "@/assets/arrow-right.svg";
+import Logo from "@/assets/logosaas.png";
+import MenuIcon from "@/assets/menu.svg";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
+        <p className="text-white/60 hidden md:block">
+          Streamline your workflow and boost your productivity.
+        </p>
+        <Link href="/" className="inline-flex gap-1 items-center">
+          <p>Get started for free</p>
+          <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
+        </Link>
+      </div>
+
+      <div className="py-5">
+        <div className="container">
+          <div className="flex items-center justify-between">
+            <Image src={Logo} alt="Logo" width={40} height={40} />
+            <MenuIcon className="h-5 w-5 md:hidden" />
+            <nav className="hidden md:flex gap-6 text-black/60 items-center">
+              <Link href="/">About</Link>
+              <Link href="/">Features</Link>
+              <Link href="/">Customers</Link>
+              <Link href="/">Updates</Link>
+              <Link href="/">Help</Link>
+              <Link
+                href="/"
+                className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex items-center justify-center tracking-tight"
+              >
+                Get for free
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
+
+export default Header;
